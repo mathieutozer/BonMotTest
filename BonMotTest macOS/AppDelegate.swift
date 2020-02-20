0321b455c702b1b82767ba8ca7bd6bcdb4c52602
Mathieu Tozer <mathieutozer@gmail.com>
2020-02-20 21:40:25 +0100

    Initial Commit

---
+++ BonMotTest macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BonMotTest macOS
+//
+//  Created by Mathieu Tozer on 2/20/20.
+//  Copyright © 2020 Tozer LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
